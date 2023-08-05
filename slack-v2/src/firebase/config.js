be64a2d1ae3e4796a4ab0754da1326371ac68ade
Abdullah <abdullahyaqoob3423@gmail.com>
2023-08-05 18:39:56 +0500

    firebase initialization done

---
+++ slack-v2/src/firebase/config.js
@@ -0,0 +1,24 @@
+import firebase from 'firebase/app';
+import 'firebase/auth'
+import 'firebase/firestore'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBlTYsDZnG6oWl1MKzwlLQzKy566O9xrOQ",
+  authDomain: "slack-v2-5924a.firebaseapp.com",
+  projectId: "slack-v2-5924a",
+  storageBucket: "slack-v2-5924a.appspot.com",
+  messagingSenderId: "718137822219",
+  appId: "1:718137822219:web:060378afeb4b1812644b0e"
+};
+
+// Initialize firebase
+firebase.initializeApp(firebaseConfig);
+
+// Initilize firebase services
+const projectFirestore = firebase.firestore();
+const projectAuth = firebase.auth();
+
+// TimeStamp
+const timeStamp = firebase.firestore.Timestamp
+
+export { projectFirestore , projectAuth , timeStamp }
