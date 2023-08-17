39330417db6b4270527f52f0795754d57c16ef90
Abdullah <abdullahyaqoob3423@gmail.com>
2023-08-17 11:29:36 +0500

    Create new project page done

--- slack-v2/src/pages/newProject/NewProject.js
+++ slack-v2/src/pages/newProject/NewProject.js
@@ -1,8 +1,103 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import Select from 'react-select'
+import { useGetData } from '../../customHooks/useGetData'
+import './NewProject.css'
+
+const categories = [
+  {value:'requirement gathering' , label:'Requirement Gathering'},
+  {value:'designing' , label:'Designing'},
+  {value:'development' , label:'Development'},
+  {value:'verification & validation' , label:'Verification & Validation'},
+  {value:'testing' , label:'Testing'},
+  {value:'deployment' , label:'Deployment'},
+]
 
 function NewProject() {
+
+  const { documents } = useGetData('users');
+  const [users, setUsers] = useState([]);
+  
+  const [name, setName] = useState('');
+  const [details, setDetails] = useState('');
+  const [dueDate, setDueDate] = useState('');
+  const [category, setCategory] = useState('');
+  const [assignedUsers, setAssignedUsers] = useState([]);
+
+  const [errors, setErrors] = useState(null)
+
+  useEffect(() => {
+    if (documents) {
+      const options = documents.map(user => {
+        return { value: user , label: user.displayName }
+      })
+      setUsers(options)
+    }
+  }, [documents])
+  
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    setErrors(null);
+    if (!category) {
+      setErrors("Please select the project category")
+      return
+    }
+    if (assignedUsers.length < 1) {
+      setErrors("Please select atleast one user")
+      return
+    }
+    console.log(name,details,dueDate,category.value, assignedUsers);
+  }
+
   return (
-    <div>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsam sapiente assumenda aliquam repellendus esse, officiis ullam quo tempora aut laudantium placeat autem delectus, veniam numquam doloribus perferendis deserunt odio libero.</div>
+    <>
+      <div className="create">
+        <h2 className='createTitle'>Create a new project</h2>
+
+        <form onSubmit={handleSubmit}>
+          <label>
+            <span>Project Name:</span>
+            <input type="text" value={name} onChange={e => setName(e.target.value)} required />
+          </label>
+
+          <label>
+            <span>Project Description:</span>
+            <textarea type="text" value={details} onChange={e => setDetails(e.target.value)} required />
+          </label>
+
+          <label>
+            <span>Set due date:</span>
+            <input type="date" value={dueDate} onChange={e => setDueDate(e.target.value)} required />
+          </label>
+
+          <label >
+            <span>Project Category:</span>
+            <Select
+            className='select'
+            options={categories}
+            onChange={option => { 
+              setCategory(option)
+              setErrors(null)
+            }} />
+          </label>
+
+          <label>
+            <span>Assign to:</span>
+            <Select
+            onChange={option => {
+              setAssignedUsers(option)
+              setErrors(null)
+              }}
+            options={users}
+            isMulti />
+          </label>
+
+          <button className='btn'>Add Project</button>
+          {errors && <div className="error">{errors}</div> }
+        </form>
+
+      </div>
+    </>
   )
 }
 
--- slack-v2/src/pages/projects/Projects.js
+++ slack-v2/src/pages/projects/Projects.js
@@ -2,7 +2,26 @@ import React from 'react'
 
 function Projects() {
   return (
-    <div>Projects</div>
+    <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus iste eius deleniti, modi laudantium commodi fuga assumenda recusandae qui explicabo consectetur corrupti perspiciatis, alias placeat facere labore autem ipsa repellat.
+    Cumque tempore odit reprehenderit fuga quidem molestias, ullam aliquid ea iusto repudiandae a. Aut fuga labore cumque eius ex beatae ratione magni enim quisquam nemo, possimus deleniti reprehenderit non eveniet?
+    Doloribus explicabo nam quibusdam labore fugit omnis sed, nihil dolorum eligendi, quod est odio magni corporis quia voluptate. Nobis nam dolorem ea eaque officia dolore corporis animi, labore minus asperiores.
+    Quia, quae ipsam iusto vel, eveniet modi, obcaecati sapiente est non impedit veritatis eaque id. Iusto, aperiam exercitationem enim cupiditate sapiente distinctio dolor suscipit, facere nihil molestiae obcaecati nostrum minima.
+    Laborum beatae magnam fuga ex id eveniet? Vero doloremque autem, obcaecati quaerat ipsum assumenda aliquid voluptatem neque quasi non deleniti dolores nostrum quia iure, accusamus facilis ducimus similique veritatis nihil!
+    Dignissimos maxime cupiditate iure consequuntur harum unde! Et ullam esse fugit minima, temporibus culpa non assumenda! Voluptas alias architecto, suscipit aliquam repellendus exercitationem, reprehenderit tempora veniam voluptatem, itaque quos dolorum!
+    Aut obcaecati repudiandae maiores similique illum, deserunt delectus ducimus ex praesentium voluptas necessitatibus eligendi corrupti saepe, et corporis. Veniam sapiente repellendus quaerat nam, aspernatur sit quidem dicta molestias. Atque, dolorum!
+    Reiciendis similique minima veritatis quo perspiciatis ipsum laudantium laboriosam perferendis architecto. Labore architecto tenetur sed soluta sit! Minus, esse in! Veniam harum mollitia facere, rerum commodi amet rem. Hic, eaque.
+    Porro facilis adipisci tenetur excepturi saepe magni, sequi rem aspernatur quibusdam at, perspiciatis cupiditate nulla totam neque aliquam? Minus similique ut saepe minima architecto officia sed est quod! Commodi, ducimus?
+    Iusto animi, ipsum ducimus sapiente id, unde esse, eveniet nesciunt aperiam modi nisi. Facere aspernatur possimus in quis. Architecto enim perferendis numquam dolore illo ea culpa quae. Perferendis, mollitia eveniet!
+    Sit quos aspernatur nobis tempora, expedita rem? Iusto iure voluptas quam sint. Aspernatur vitae accusantium, ducimus modi officiis consectetur ad facilis perferendis quisquam quos, excepturi ex, quod impedit. Est, dolor.
+    Saepe aliquam nostrum, blanditiis in animi dolores aspernatur, sapiente quasi sint, cumque reiciendis et. Illo deleniti deserunt provident quibusdam, ipsa ipsum adipisci. Maxime quae, sed fugiat vitae numquam ex unde.
+    Ipsa iste pariatur maxime dolorum qui tenetur quod obcaecati veniam ratione quas cumque similique, culpa tempore officiis optio corporis laboriosam! Ducimus qui perspiciatis iure reprehenderit facilis, nobis temporibus praesentium dolores.
+    Consectetur animi atque inventore dolorum nostrum, dolorem mollitia nam unde? Exercitationem ipsa illo vel, eum magni delectus rerum culpa, quis eligendi, quisquam tenetur voluptatibus alias aut ut doloribus eius tempora.
+    Voluptatum recusandae vel voluptatibus tempore quos qui laboriosam quas culpa laudantium, illum voluptates porro ab minima accusamus hic incidunt quod quaerat? Quia consequuntur esse ipsa numquam debitis corporis enim omnis?
+    Delectus nihil fugit molestiae repellat minima quibusdam id beatae consequuntur consectetur autem accusantium cum, ipsum quos voluptates ab. Quaerat quasi praesentium quibusdam quia dolores ut libero ratione distinctio optio doloribus!
+    Nam quis repudiandae accusamus harum nostrum labore doloribus minima possimus! Possimus officia obcaecati voluptatem amet, dicta deserunt modi quam optio rerum ipsa, vero accusamus odio a? Dolorum rerum optio magni.
+    Nostrum porro nisi illo optio ab autem vero inventore eum ipsa? Earum quod rerum quisquam laborum sit doloremque pariatur impedit totam magni fugit unde repellendus, iste sed quasi voluptatem incidunt?
+    Eos ex temporibus veniam ut! Dolores provident eveniet quam quidem ab labore atque, alias corrupti accusantium! Nesciunt enim maxime distinctio voluptatibus magni consequuntur ducimus. Voluptatibus porro minima earum quos! Laboriosam?
+    Ullam debitis necessitatibus aliquam quod. Iure maiores sapiente aut in recusandae inventore! Dignissimos nostrum alias repudiandae ullam eum, distinctio reprehenderit dolore ipsa a doloribus autem facilis quis, dicta expedita eius!</div>
   )
 }
 
