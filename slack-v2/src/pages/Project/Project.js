19642a858e4906ef97e29d63301bdda1829ee9ab
Abdullah <abdullahyaqoob3423@gmail.com>
2023-08-18 20:30:24 +0500

    project summary done

--- slack-v2/src/pages/Project/Project.js
+++ slack-v2/src/pages/Project/Project.js
@@ -1,8 +1,37 @@
 import React from 'react'
 import './Project.css'
+import Avatar from '../../components/Avatar'
+import { Icon } from '@iconify/react';
+import { useParams } from "react-router-dom"
+import { useDocument } from '../../customHooks/useDocument';
 
 export default function Project() {
+  const { id } = useParams();
+  const { error, document } = useDocument('projects', id)
+
+  if (error) {
+    return <div className="error">{error}</div>
+  }
+  if (!document) {
+    return <div className="loading">Loading....</div>
+  }
+
   return (
-    <div>Project</div>
+    <div className='projectDetails'>
+      <h3>{document.name}</h3>
+      <span className='details'>{document.details}</span>
+      <span className='category'>{document.category}</span>
+
+      <div className="assignedTo">
+        <p><Icon icon="game-icons:sands-of-time" /> {document.dueDate.toDate().toDateString()}</p>
+        <ul>
+          {document.assignedUsersList.map(user => (
+            <li key={user.photoURL}>
+              <Avatar photo={user.photoURL} />
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
   )
 }
